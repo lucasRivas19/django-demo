f1b82421fc9f48231a5ab72cc54d6898688d61b6
Lucas Rivas <lucasrivas427@gmail.com>
2025-08-29 17:09:58 -0300

    deploy: prueba

--- core/views.py
+++ core/views.py
@@ -1,4 +1,4 @@
 from django.http import HttpResponse
 
 def hello(request):
-    return HttpResponse("Hi!!. Estoy de prueba de Deploy usando Jenkins Waven. 👋")
+    return HttpResponse("Hi!!. Deploy usando Jenkins Waven. 👋")
