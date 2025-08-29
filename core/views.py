1429753fa66549b1dc8e0adac5f3b770401b136b
Lucas Rivas <lucasrivas427@gmail.com>
2025-08-29 13:49:11 -0300

    mi primer cambio para Jenkinsfile

--- core/views.py
+++ core/views.py
@@ -1,4 +1,4 @@
 from django.http import HttpResponse
 
 def hello(request):
-    return HttpResponse("Hola. es la prueba de Deploy usando Jenkins. 👋")
+    return HttpResponse("Hi!!. Es la prueba de Deploy usando Jenkins. 👋")
