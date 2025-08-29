e92743fc79c40db31c854c72fdbfa2ae8f15d87b
Lucas Rivas <lucasrivas427@gmail.com>
2025-08-29 16:43:10 -0300

    deploy: prueba con tag v1.6.2

--- core/views.py
+++ core/views.py
@@ -1,4 +1,4 @@
 from django.http import HttpResponse
 
 def hello(request):
-    return HttpResponse("Hi!!. Es la prueba de Deploy usando Jenkins Waven. 👋")
+    return HttpResponse("Hi!!. prueba de Deploy usando Jenkins Waven. 👋")
